f30a891d6ab533bd5135e1606f554fb5a3eaa38a
Arunachalam <arunachalam@crayond.co>
2024-09-04 20:12:02 +0530

    initial commit for backend

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 6000;
 
 // Import the items controller.js
 const itemsController = require("./controller");
